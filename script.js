f3ef0c5515f911d9477d1c9b11162f0b32b44ff6
Mark Nicol <marc.nuc@gmail.com>
2023-01-20 12:41:10 -0500

    first change

---
+++ script.js
@@ -0,0 +1,6 @@
+function myChange() {
+    document.querySelector(".name").style.fontSize="64px"
+    document.querySelector("body").style.backgroundColor="black"
+    document.querySelector(".name").innerHTML="World Changed!"
+    document.querySelector("button").style.display="none"
+}
